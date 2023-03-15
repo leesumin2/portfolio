62da8c4eb7b8d896c970595986feb479a63a8b1f
LEE SuMin <tnals2322@gmail.com>
2023-03-15 18:23:10 +0900

    JS : Navbar

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-// Navbar
-document.addEventListener('scroll', function () {
-    let currentScrollValue = document.documentElement.scrollTop;
-    let navbar = document.getElementById("navbar");
-    if (currentScrollValue > 40) {
-        navbar.style.backgroundColor = "#B2CCFF";
+'use strict';
+
+// 스크롤에 따른 navbar 색 변경
+const navbar = document.querySelector('#navbar');
+// 브라우저에서 보여지는 width, height 받아와야하기 때문에
+// getBoundingClientRect() 사용.
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--change-color');
     } else {
-        navbar.style.backgroundColor = "#bbddf8";
+        navbar.classList.remove('navbar--change-color');
     }
 });
