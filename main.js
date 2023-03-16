2331b7869a2c11c1e670ef3ac83d1aa29caea990
LEE SuMin <tnals2322@gmail.com>
2023-03-16 13:37:53 +0900

    JS : Home contact button

--- main.js
+++ main.js
@@ -21,8 +21,16 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
-    // window.scrollBy(0, -50);
+    scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const contactMe = document.querySelector('.home__contact');
+contactMe.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+}
